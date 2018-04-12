afb104e1b32854cb0cf068e9960bec32bc3d9f3a
Atsushi Nagase <a@ngs.io>
2018-04-12 10:55:10 +0900

    Update asset_hash ignore

--- config.rb
+++ config.rb
@@ -127,7 +127,7 @@ end
 configure :build do
   activate :minify_css
   activate :minify_javascript
-  activate :asset_hash, ignore: 'images', exts: %w(.woff2 .eot .svg .ttf .woff .otf .css .js .jpg .png .gif)
+  activate :asset_hash, ignore: 'images/**/*', exts: %w(.woff2 .eot .svg .ttf .woff .otf .css .js .jpg .png .gif)
   ignore '.DS_Store'
   ignore '.*.swp'
   ignore '_drafts'
